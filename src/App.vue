58ca2cc9a0fa8468bbd3d600a7bc6a0530ddbefe
Ray Jiang <CtD8864@gmail.com>
2021-12-09 00:07:24 +0800

    set up api key

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
+    Hello
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      api_key: 'be2fb405139532b7b5132b29f8328d4c'
+    }
+  }
 }
+
 </script>
 
 <style>
